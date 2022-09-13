f53fa8011ecbbe97c69a241c491a5091d06d6689
Tyler Watson (He/Him) <107147939+tylerww91@users.noreply.github.com>
2022-09-13 14:19:23 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+/* Imports */
+
+/* State */
+
+/* Actions */
+function load() {}
+
+/* Components */
+
+/* Component */
+// get DOM
+// display
+// event listeners
+
+/* Run page load code */
+load();
