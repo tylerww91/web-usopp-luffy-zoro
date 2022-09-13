9c748ecb24453e0f271d356480dbd96b437c42e2
Tyler Watson <tylerww91@gmail.com>
2022-09-13 16:52:08 -0700

    adds event listeners for all 3 buttons, adds let variables for states, checked to make sure choose/results state appear differently on web page, adds util.js with imported functions

--- app.js
+++ app.js
@@ -2,31 +2,60 @@
 
 /* State */
 let gameState = 'choose';
-
+let userChoice = '';
+let computerChoice = '';
+let wins = 0
+let losses = 0
+let draws = 0
+let total = 0
 
 /* Actions */
-function load() {}
-chooseFighter();
+function load() {
+    chooseFighter();
+}
 /* Components */
 
 /* Component */
 // get DOM
-const usoppImg = document.getElementById('usopp-img')
-const luffyImg = document.getElementById('luffy-img')
-const zoroImg = document.getElementById('zoro-img')
+const usoppImg = document.getElementById('usopp-img');
+const luffyImg = document.getElementById('luffy-img');
+const zoroImg = document.getElementById('zoro-img');
 
-const chooseUsopp = document.getElementById('usopp');
-const chooseLuffy = document.getElementById('luffy');
-const chooseZoro = document.getElementById('zoro');
+const chooseUsopp = document.getElementById('usopp-button');
+const chooseLuffy = document.getElementById('luffy-button');
+const chooseZoro = document.getElementById('zoro-button');
 
+const playAgain = document.getElementById('play-again-button');
 
 // display
 function chooseFighter() {
-    if (gameState === 'choose')
-    
+    if (gameState === 'choose') {
+        playAgain.classList.add('hidden');
+    } else {
+        if ()
+    }
 }
 
 // event listeners
+
+chooseUsopp.addEventListener('click', () => {
+    userChoice = 'usopp-button';
+    chooseFighter();
+    gameState = 'results';
+});
+
+chooseLuffy.addEventListener('click', () => {
+    choose = 'luffy-button';
+    chooseFighter();
+    gameState = 'results';
+});
+
+chooseZoro.addEventListener('click', () => {
+    choose = 'zoro-button';
+    chooseFighter();
+    gameState = 'results';
+});
+
 
 /* Run page load code */
 load();
