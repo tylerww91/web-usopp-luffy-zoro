abeede6f9a6e360c33693bf346c7ecdba0672695
Tyler Watson <tylerww91@gmail.com>
2022-09-13 16:09:28 -0700

    adds luffy img, zoro img, usopp img, adds css classes for luffy-img and usopp-img, adds const variables for usoppImg luffyImg and zoroImg as well as 3 buttons chooseUsopp chooseLuffy chooseZoro. let variable for gameState

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 /* Imports */
 
 /* State */
+let gameState = 'choose';
+
 
 /* Actions */
 function load() {}
-
+chooseFighter();
 /* Components */
 
 /* Component */
 // get DOM
+const usoppImg = document.getElementById('usopp-img')
+const luffyImg = document.getElementById('luffy-img')
+const zoroImg = document.getElementById('zoro-img')
+
+const chooseUsopp = document.getElementById('usopp');
+const chooseLuffy = document.getElementById('luffy');
+const chooseZoro = document.getElementById('zoro');
+
+
 // display
+function chooseFighter() {
+    if (gameState === 'choose')
+    
+}
+
 // event listeners
 
 /* Run page load code */
